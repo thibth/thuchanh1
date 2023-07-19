a5e2aeeef44be2d9572e53ab91bf0bd4c22aa4ac
FireGroup <FireGroup@192.168.101.250>
2023-07-19 16:50:11 +0700

    upload

---
+++ Test Suites/ví dụ.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ví dụ</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bbdd3149-dfa4-4c12-a511-e8c33ea0580c</testSuiteGuid>
+   <testCaseLink>
+      <guid>9031dd89-a13b-4dd2-888d-49890ad2ae8c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bt1_login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5668ea11-4cd4-4815-8170-4c8255fec808</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bt2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>dcce83cd-2879-4746-af73-bd4dd4e4a01b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e37c71d9-7e76-4c48-b9d8-b285af6a7c4e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
